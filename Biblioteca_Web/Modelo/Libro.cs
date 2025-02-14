d211af1db7ab67be18c3bcd68da0e90be292a94a
Fabricio Sandoval <sandovalfabri2023@gmail.com>
2025-02-14 15:55:11 -0600

    Agregar archivos de proyecto.

---
+++ Biblioteca_Web/Modelo/Libro.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace Biblioteca_Web.Modelo
+{
+    public class Libro
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Titulo { get; set; }
+        public DateTime AnoPublicacion { get; set; }
+
+        public int? Autor_id { get; set; }
+        public int? Categoria_id { get; set; }
+        public string Resumen { get; set; }
+    }
+}
